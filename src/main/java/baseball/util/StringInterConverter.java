b4f1aca1e13228c9958e5391e76dbc0b7eac10ac
Park, Sojeong <sojeong2101@gmail.com>
2024-04-04 00:34:22 +0900

    :art: [style] 파일의 마지막 새줄 문자 LF로 변경

--- src/main/java/baseball/util/StringInterConverter.java
+++ src/main/java/baseball/util/StringInterConverter.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringInterConverter {
+
     public static List<Integer> convertStringToIntegers(final String input) {
         return new ArrayList<>(input.chars()
                 .map(Character::getNumericValue)
